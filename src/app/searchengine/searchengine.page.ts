ac65989949aea8f957a0fdd1734eb2078bdcfe85
Daniel Almazan <almazan19daniel@gmail.com>
2023-04-06 20:19:53 +0800

    Search Engine Page Created
    User clicks search icon => go to searchengine page
    
    future function:
    if user is not loggedin/doesnt have account = redirect to login
    if user is logged in = user will proceed to job application and submit resume

---
+++ src/app/searchengine/searchengine.page.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-searchengine',
+  templateUrl: './searchengine.page.html',
+  styleUrls: ['./searchengine.page.scss'],
+})
+export class SearchenginePage implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
