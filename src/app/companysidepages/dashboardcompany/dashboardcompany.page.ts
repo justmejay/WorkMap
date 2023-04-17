bd786c95cab5e7ff3d2586d8de3fcad95fc476af
Nicole Carlet <nicolecarlet4@gmail.com>
2023-04-17 15:12:05 +0800

    addjoblisting

--- src/app/companysidepages/dashboardcompany/dashboardcompany.page.ts
+++ src/app/companysidepages/dashboardcompany/dashboardcompany.page.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { AuthService } from 'src/app/services/auth.service';
+
 
 @Component({
   selector: 'app-dashboardcompany',
@@ -7,9 +10,20 @@ import { Component, OnInit } from '@angular/core';
 })
 export class DashboardcompanyPage implements OnInit {
 
-  constructor() { }
+  constructor(
+    private auth: AuthService,
+    private router: Router,
+  ) { }
 
   ngOnInit() {
+  }
+
+  
+  logout(){
+    this.auth.logout();
+    this.router.navigate(['authentication'], );
+
+    
   }
 
 }
