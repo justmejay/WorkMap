b1c4b9b86c8122b4d018ed6775bbe0b541995842
Jay Ucol <jayucol1527@gmail.com>
2023-10-22 08:58:07 +0800

    Update environment.prod.ts
    removed api

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,14 +1,8 @@
 export const environment = {
   production: true,
   firebase: {
-    apiKey: "AIzaSyAX5WtEhz9MTvU3CPOMqAF5rllm7AM9A1o",
-  authDomain: "workmap-1c55b.firebaseapp.com",
-  projectId: "workmap-1c55b",
-  storageBucket: "workmap-1c55b.appspot.com",
-  messagingSenderId: "626136246063",
-  appId: "1:626136246063:web:a205b4cf61884537cf63da",
-  measurementId: "G-68KHL0ZBJ7"
+    //add firebase api
   },
-  google_maps_api_key: "AIzaSyALpfIu1v6INFmCf8GYJSRY9iteqqq2Fbc",
+  google_maps_api_key: "", //add google maps api
 
 };
